dcadd45277b9013a6ed0abfcc2999e03e9175e58
Rodolfo Sousa <rodolfolopes1996@hotmail.com>
2021-11-17 14:00:55 -0300

    Delete-Deletando coisas que não estão na master mais

--- Codigo/js/scriptsMensagem.js
+++ Codigo/js/scriptsMensagem.js
@@ -7,7 +7,7 @@ function achaUsuarioAtual(Id, usuariodb) {
     }
     return false;//Se caiu aqui,usuario não está logado,e portanto não deve estar aqui
 }
-function achaNomePorId(ID,DB){
+function achaNomePorId(ID, DB) {
     let resposta = "erro";
     for (i = 0; i < DB.usuarios.length; i++) {
         let usuario = usuariodb.usuarios[i];
@@ -20,44 +20,44 @@ function achaNomePorId(ID,DB){
 }
 function adicionaUsuariosDropdown(objDados) {
     let dropdown = document.getElementById("usuariosDrop");
-    dropdown.innerHTML ='<option selected disabled value = 0>Selecione para quem a mensagem é:</option>';
-    for ( i = 0; i < objDados.usuarios.length; i++) {
+    dropdown.innerHTML = '<option selected disabled value = 0>Selecione para quem a mensagem é:</option>';
+    for (i = 0; i < objDados.usuarios.length; i++) {
         let option = document.createElement('option');
         let text = document.createTextNode(objDados.usuarios[i].nome);
         option.appendChild(text);
         dropdown.appendChild(option);
-        option.setAttribute('value',objDados.usuarios.usuario_id);
+        option.setAttribute('value', objDados.usuarios.usuario_id);
     }
 }
-function validarMensagem(){
+function validarMensagem() {
     let drop = document.getElementById('usuariosDrop');
     let titulo = document.getElementById('tituloMensagem');
-    let corpo =document.getElementById('corpoMensagem');
-    if(drop.options[drop.selectedIndex].value == 0 || 
-    titulo.value.length==0 || 
-    corpo.value.length==0 ){
+    let corpo = document.getElementById('corpoMensagem');
+    if (drop.options[drop.selectedIndex].value == 0 ||
+        titulo.value.length == 0 ||
+        corpo.value.length == 0) {
         alert('Pelo menos um dos valores não foi preenchido!');
-    }else{
+    } else {
         let pessoaID = drop.options[drop.selectedIndex].value;
-        criarMensagem(titulo,corpo,pessoaID);
+        criarMensagem(titulo, corpo, pessoaID);
         titulo.value = '';
         corpo.value = '';
     }
 }
-function criarMensagem(titulo,corpo,IDDestinatario){
+function criarMensagem(titulo, corpo, IDDestinatario) {
     let IDUsuario = localStorage.getItem("usuarioAtual");
     let dbMensagens = JSON.parse(localStorage.getItem("dbMensagens"));
-    
-    if(!dbMensagens){
+
+    if (!dbMensagens) {
         criardbMensagem(IDUsuario);
     }
-    let novaMensagem = 
+    let novaMensagem =
         { de: IDUsuario, para: IDDestinatario, titulo: titulo, mensagem: corpo }
-    
-        dbMensagens.mensagens.push(novaMensagem);
-        salvaDBMensagen(dbMensagens);
+
+    dbMensagens.mensagens.push(novaMensagem);
+    salvaDBMensagen(dbMensagens);
 }
-function criardbMensagem(){
+function criardbMensagem() {
     let parser = localStorage.getItem("db");
     let objDados = JSON.parse(parser);
     let id0 = objDados.usuarios[0].usuario_id;
@@ -67,13 +67,27 @@ function criardbMensagem(){
         mensagens: [
             { de: id0, para: id1, titulo: "Mensagem Teste-1", mensagem: "Teste corpo da mensagem 1." },
             { de: id1, para: id2, titulo: "Mensagem Teste-2", mensagem: "Teste corpo da mensagem 2." },
-            { de: id2, para:id0, titulo: "Mensagem Teste-3", mensagem: "Teste corpo da mensagem 3." }
+            { de: id2, para: id0, titulo: "Mensagem Teste-3", mensagem: "Teste corpo da mensagem 3." }
         ]
     }
     salvaDBMensagen(Mensagens);
 }
-function salvaDBMensagen(DB){
-    localStorage.setItem("dbMensagens",JSON.stringify(DB));
+function salvaDBMensagen(DB) {
+    localStorage.setItem("dbMensagens", JSON.stringify(DB));
+}
+function dropdownMensagens(DBUsuarios, IDAtual) {
+    let parser = localStorage.getItem("dbMensagens");
+    let dbMensagens = JSON.parse(parser);
+    let temMensagem = false;
+    for (i = 0; i < dbMensagens.mensagens.length; i++) {
+        if (IDAtual == dbMensagens.mensagens[i].para) {
+            temMensagem = true;
+        }
+    }
+    if(temMensagem){
+        let desaparecer = document.getElementById("desaparecerDepois");
+        desaparecer.setAttribute('hidden',true);
+    }
 }
 window.onload = function () {
     if (!(localStorage.getItem("db") === null)) {
@@ -86,10 +100,11 @@ window.onload = function () {
             alert("É necessário logar antes!");
             document.location.href = "index.html";
 
-        }else{
+        } else {
             adicionaUsuariosDropdown(objDados);
+            dropdownMensagens(objDados, IDUsuario);
             let dbMensagens = localStorage.getItem("dbMensagens");
-            if(!dbMensagens){
+            if (!dbMensagens) {
                 criardbMensagem(IDUsuario);
             }
         }
